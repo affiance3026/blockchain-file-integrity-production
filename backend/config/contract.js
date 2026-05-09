86dd01999c23e2068c2ee8bfaeb086ce4aff24db
affiance3026 <www.sallu3026@gmail.com>
2026-05-09 17:54:20 +0530

    Assigned ABI directly

--- backend/config/contract.js
+++ backend/config/contract.js
@@ -11,7 +11,104 @@ const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 const contractAddress = process.env.CONTRACT_ADDRESS;
 
 // ABI
-const abi = require("../../blockchain/artifacts/contracts/CertificateRegistry.sol/CertificateRegistry.json").abi;
+const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "certificates",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "certificate_id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "cid",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "issuer_id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "user_id",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_certificate_id",
+          "type": "string"
+        }
+      ],
+      "name": "getCertificate",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_certificate_id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_cid",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_issuer_id",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_user_id",
+          "type": "string"
+        }
+      ],
+      "name": "storeCertificate",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+]
 
 // Contract instance
 const contract = new ethers.Contract(contractAddress, abi, wallet);
